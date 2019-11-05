5349bebc20e73ac2113ac00c6f12299b1c52db23
aaronsomo <aaroneight07@gmail.com>
2019-11-04 20:50:48 -0800

    need to add buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,79 +5,58 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 
 
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em>view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[1]}/>
-      </div>
-      <div className="col-md-5">
-        <div>
-          <VideoList videos={exampleVideoData}/>
+// var App = (props) => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em>view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video={exampleVideoData[1]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <div>
+//           <VideoList videos={exampleVideoData}/>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em>view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={exampleVideoData[1]}/>
+          </div>
+          <div className="col-md-5">
+            <div>
+              <VideoList videos={exampleVideoData}/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-
-//     };
-//   }
-// }
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
-
-// import exampleVideoData from '../data/exampleVideoData.js';
-// import VideoList from './VideoList.js';
-// import VideoPlayer from './VideoPlayer.js';
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       selectedTitle: null,
-//       selectedDescription: null,
-//       selectedUrl: null
-//     };
-//   }
-
-//   onVideoListClick() {
-
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <nav className="navbar">
-//           <div className="col-md-6 offset-md-3">
-//             <div><h5><em>search</em> fuck off</h5></div>
-//           </div>
-//         </nav>
-//         <div className="row">
-//           <div className="col-md-7">
-//             <VideoPlayer video={exampleVideoData[0]} />
-//           </div>
-//           <div className="col-md-5">
-//             <VideoList videos={exampleVideoData}/>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// // In the ES6 spec, files are "modules" and do not share a top-level scope
-// // `var` declarations will only exist globally where explicitly defined
-// export default App;
